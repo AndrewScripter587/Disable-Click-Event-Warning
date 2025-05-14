bf05f5bd16c204175ba51309a470ed4d84f842b7
AndrewScripter587 <145153158+AndrewScripter587@users.noreply.github.com>
2025-05-14 11:55:26 -0400

    use the fabric example mod as a base

---
+++ src/client/java/com/andrewgaming/nocewarn/DisableClickEventWarningClient.java
@@ -0,0 +1,10 @@
+package com.andrewgaming.nocewarn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DisableClickEventWarningClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
